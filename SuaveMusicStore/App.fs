92e91c15ec955a015fee646f5bd1cfebe892ff8f
vikranth <vikranth@outlook.com>
2016-02-21 09:09:43 -0700

    Routing

--- SuaveMusicStore/App.fs
+++ SuaveMusicStore/App.fs
@@ -1,4 +1,15 @@
 ﻿open Suave
 open Suave.Successful
+open Suave.Operators
+open Suave.Web
+open Suave.Filters
 
-startWebServer defaultConfig (OK "Hello world!")
+let webPart : WebPart =
+    choose [
+        path "/" >=> OK "Home"
+        path "/store" >=> OK "Store"
+        path "/store/brows" >=> OK "Store"
+        path "/store/details" >=> OK "Details"
+    ]
+
+startWebServer defaultConfig webPart
